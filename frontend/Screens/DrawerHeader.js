8bb8829487aeed90a92f602d2565f194620731fd
reason-knowledge <franckhervengoubounkou@gmail.com>
2024-05-30 12:02:37 +0100

    all files

---
+++ frontend/Screens/DrawerHeader.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { DrawerContentScrollView, DrawerItemList } from "@react-navigation/drawer";
+
+const DrawerHeader = (props) => (
+  <DrawerContentScrollView {...props}>
+    <View style={styles.header}>
+      <View style={styles.profileCircle}>
+        <Image source={require('../Assets/image6.jpg')} style={styles.profileImage} />
+      </View>
+      <Text style={styles.text}>Faith Gaiciumia</Text>
+    </View>
+    <DrawerItemList {...props} />
+  </DrawerContentScrollView>
+);
+
+const styles = StyleSheet.create({
+  header: {    
+    height: 150,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  profileCircle: {
+    width: 100,
+    height: 100,
+    backgroundColor: "#12B886",
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: 'hidden', // Ensures the image is clipped to the container's bounds
+  },
+  profileImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover', // Ensures the image covers the container without stretching
+  },
+  text: {
+    marginTop: 10,
+    fontSize: 18,
+  },
+});
+
+export default DrawerHeader;
--- frontend/Screens/drawerProfessors.js
+++ frontend/Screens/drawerProfessors.js
@@ -1,30 +1,17 @@
 import React from 'react';
-<<<<<<< HEAD
 import { View, StyleSheet } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-// import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-// import HomeProfessors from './homeProfessors';
-// import AllUsers from './allUsers';
-// import AllUsersConnected from './allUsersConnected';
-// import GradeStudents from './gradeStudents';
-// import DrawerHeader from './DrawerHeader';
-=======
-import { StyleSheet ,View ,Text } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import HomeProfessors from './homeProfessors';
 import AllUsers from './allUsers';
 import AllUsersConnected from './allUsersConnected';
 import GradeStudents from './gradeStudents';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-
->>>>>>> ea830d4bc3a7a60d6fdfb7a189e3c9db45ff9957
+import DrawerHeader from './DrawerHeader';
 
 const Drawer = createDrawerNavigator();
 
 const DrawerProfessors = () => {
   return (
-<<<<<<< HEAD
     <Drawer.Navigator drawerContent={(props) => <DrawerHeader {...props} />}>
       <Drawer.Screen
         name="HomeProfessors"
@@ -37,47 +24,23 @@ const DrawerProfessors = () => {
         options={{
           drawerIcon: ({ color }) => (
             <View style={styles.iconContainer}>
-=======
-    <Drawer.Navigator>
-      <Drawer.Screen
-        name="HomeProfessors"
-        component={HomeProfessors}
-        options={{headerTitle :'',}}
-      />
-      <Drawer.Screen
-       name="AllUsersConnected"
-        component={AllUsersConnected}
-        options={{
-          drawerIcon: ({ color }) => (
-            <View style={{ backgroundColor:'#D9D9D9' ,borderRadius:20,width:40,height:40,justifyContent:'center',alignItems:'center'}}>
->>>>>>> ea830d4bc3a7a60d6fdfb7a189e3c9db45ff9957
               <MaterialCommunityIcons name="account-group" size={30} color="black" />
             </View>
           ),
         }}
-<<<<<<< HEAD
       />
       <Drawer.Screen
-=======
-        />
-        <Drawer.Screen
->>>>>>> ea830d4bc3a7a60d6fdfb7a189e3c9db45ff9957
         name="AllUsers"
         component={AllUsers}
         options={{
           drawerIcon: ({ color }) => (
-<<<<<<< HEAD
             <View style={styles.iconContainer}>
-=======
-            <View style={{ backgroundColor: '#D9D9D9', borderRadius: 20, width: 40, height: 40, justifyContent: 'center', alignItems: 'center' }}>
->>>>>>> ea830d4bc3a7a60d6fdfb7a189e3c9db45ff9957
               <MaterialCommunityIcons name="account-group" size={30} color="black" />
             </View>
           ),
         }}
       />
       <Drawer.Screen
-<<<<<<< HEAD
         name="GradeStudents"
         component={GradeStudents}
         options={{
@@ -102,20 +65,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 });
-=======
-      name="GradeStudents"
-      component={GradeStudents}
-      options={{
-        drawerIcon: ({ color }) => (
-          <View style={{ backgroundColor: '#D9D9D9', borderRadius: 20, width: 40, height: 40, justifyContent: 'center', alignItems: 'center' }}>
-            <MaterialCommunityIcons name="pencil" size={30} color="black" />
-          </View>
-        ),
-      }}
-    />
-    </Drawer.Navigator>
-  );
-}
->>>>>>> ea830d4bc3a7a60d6fdfb7a189e3c9db45ff9957
 
 export default DrawerProfessors;
