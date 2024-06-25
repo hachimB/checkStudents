cceb9abbbf3430450c4579d1d60538af063bf4c3
reason-knowledge <franckhervengoubounkou@gmail.com>
2024-06-25 17:27:04 +0100

    update

--- frontend/App.js
+++ frontend/App.js
@@ -7,6 +7,7 @@ import AccueilTest from './Screens/AccueilTest';
 import DiagrammeStudent  from "./Screens/statisticUser";
 import AllUsers from './Screens/allUsers';
 import AllUsersConnected from './Screens/allUsersConnected';
+import BottomNavigationCustom from "./Screens/bottomNavigation";
 import ConnectionProfessors from './Screens/connectionProfessors';
 import ConnectionStudents from './Screens/connectionStudents';
 import DrawerProfessors from './Screens/drawerProfessors';
@@ -29,13 +30,14 @@ const App = () => {
         <Stack.Screen name="AccueilTest" component={AccueilTest} />
         <Stack.Screen name="AllUsers" component={AllUsers} />
         <Stack.Screen name="AllUsersConnected" component={AllUsersConnected} />
+        <Stack.Screen name="BottomNavigationCustom" options={{headerShown: false}} component={BottomNavigationCustom} />
         <Stack.Screen name="DiagrammeStudent" component={DiagrammeStudent} />
         <Stack.Screen name="ConnectionProfessors" component={ConnectionProfessors} />
         <Stack.Screen name="ConnectionStudents" component={ConnectionStudents} />
         <Stack.Screen name="DrawerProfessors" component={DrawerProfessors} options={{headerShown : false}} />
         <Stack.Screen name="Home" component={Home} options={{headerShown : false}}  />
         <Stack.Screen name="HomeProfessors" options={{ headerTitle: '' }} component={HomeProfessors} />
-        <Stack.Screen name="HomeStudents" options={{ headerTitle: '' }}  component={HomeStudents} />
+        <Stack.Screen name="HomeStudents" options={{ headerTitle: ''}}  component={HomeStudents} />
         <Stack.Screen name="InscriptionProfessors" component={InscriptionProfessors} />
         <Stack.Screen name="InscriptionStudents" component={InscriptionStudents}  />
         <Stack.Screen name="SignUp" component={SignUp} />
