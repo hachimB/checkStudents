367aea75538ee4060f90ef40bb00acf26e1b35da
reason-knowledge <franckhervengoubounkou@gmail.com>
2024-06-25 17:27:47 +0100

    update

--- frontend/Screens/user.js
+++ frontend/Screens/user.js
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-const user = () => {
+const User = () => {
   return (
     <SafeAreaView style={{flex:1}}>
         <View>
@@ -12,6 +12,6 @@ const user = () => {
   )
 }
 
-export default user
+export default User
 
 const styles = StyleSheet.create({})
